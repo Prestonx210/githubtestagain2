3020951514527bce124f09942c48fa5f6cf687e0
Preston Harold <prestonparker893@gmail.com>
2021-04-16 12:48:27 -0400

    mmmmmm

--- githubtestagain/ViewController.swift
+++ githubtestagain/ViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     var whoIs = "talking in class?"
-    var hi = "hi"
+    var hi2 = "hi2"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
